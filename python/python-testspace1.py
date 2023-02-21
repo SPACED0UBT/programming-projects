62d18f9cee2e044b0777e25c5e5fba6bd92c5453
SPACED0UBT <d.rez123@hotmail.com>
2023-02-21 11:58:32 -0800

    More notes on argument specifiers. Stuck on a problem

--- python/python-testspace1.py
+++ python/python-testspace1.py
@@ -153,3 +153,35 @@ if big_list.count(x) == 10 and big_list.count(y) == 10:
 #i.e %s and %d
 
 #this prints out "Hello, John!"
+name = "John"
+print("Hello, %s!" % name)
+
+#you can use multiple argument specifierls using a tuple
+#this prints out "John is 23 years old."
+name = "John"
+age = 23
+print("%s is %d years old" % (name, age))
+
+#a non-string object can be formatted using the %s operator as well.
+#it will return with the "repr" method
+# This prints out: A list: [1, 2, 3]
+mylist = [1,2,3]
+print("A list: %s" % mylist)
+
+## Here are some basic argument specifiers to know
+
+## %s - String (or any object with a string representation, like numbers)
+
+## %d - Integers
+
+## %f - Floating point numbers
+
+## %.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+
+## %x/%X - Integers in hex representation (lowercase/uppercase)
+
+#This should print "Hello John Doe. Your current balance is $53.44
+data = ("John", "Doe", 53.44)
+format_string = "Hello"
+
+print("format_string, %s . Your current balance is $ %f" % (data))
