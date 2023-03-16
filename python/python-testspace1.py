191c9bdedc47f0301dbef0b38659f19cbbeb66da
SPACED0UBT <d.rez123@hotmail.com>
2023-03-16 01:44:10 -0700

    Finished basic strings

--- python/python-testspace1.py
+++ python/python-testspace1.py
@@ -230,3 +230,57 @@ print(astring[1:14:2])
 print(astring[3:7])
 print(astring[3:7:1])
 
+#instead of strrev (from C) you can reverse a string like this
+print(astring[::-1])
+
+#this prints a string with all uppercase and another all lowercase
+print(astring.upper())
+print(astring.lower())
+
+#this checks if the string starts with "Hello" or ends with "asdfasdfasdf"
+print(astring.startswith("Hello"))
+print(astring.endswith("asdfasdfasdfasdf"))
+
+#this splits the string into many substrings grouped in a list
+spacesplit = astring.split(" ")
+print(spacesplit)
+
+#Basic string operators test
+
+s = "Strings are awesome!"
+# Length should be 20
+print("Length of s = %d" % len(s))
+
+# First occurrence of "a" should be at index 8
+print("The first occurrence of the letter a = %d" % s.index("a"))
+
+# Number of a's should be 2
+print("a occurs %d times" % s.count("a"))
+
+# Slicing the string into bits
+print("The first five characters are '%s'" % s[:5]) # Start to 5
+print("The next five characters are '%s'" % s[5:10]) # 5 to 10
+print("The thirteenth character is '%s'" % s[12]) # Just number 12
+print("The characters with odd index are '%s'" %s[1::2]) #(0-based indexing)
+print("The last five characters are '%s'" % s[-5:]) # 5th-from-last to end
+
+# Convert everything to uppercase
+print("String in uppercase: %s" % s.upper())
+
+# Convert everything to lowercase
+print("String in lowercase: %s" % s.lower())
+
+# Check how a string starts
+if s.startswith("Str"):
+    print("String starts with 'Str'. Good!")
+
+# Check how a string ends
+if s.endswith("ome!"):
+    print("String ends with 'ome!'. Good!")
+
+# Split the string into three separate strings,
+# each containing only a word
+print("Split the words of the string: %s" % s.split(" "))
+
+# Conditions
+# https://www.learnpython.org/en/Conditions
